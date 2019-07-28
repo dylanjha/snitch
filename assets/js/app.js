ead05aaeb0356a428bc9b046df1325426ae35e9e
Dylan Jhaveri <dylanjhaveri@gmail.com>
2019-07-28 00:35:13 -0700

    get bootstrap set up with scss

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import "bootstrap"
+import css from "../css/app.scss"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,7 +10,6 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
 
 // Import local files
 //
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -29,8 +29,12 @@ module.exports = (env, options) => ({
         }
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
