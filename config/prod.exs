2b81e4b71c7af908372f288f5bf16707d38b586e
Dylan Jhaveri <dylanjhaveri@gmail.com>
2019-08-12 14:58:52 -0700

    force ssl on prod

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :snitch, SnitchWeb.Endpoint,
   url: [host: System.get_env("HOST_NAME"), port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
