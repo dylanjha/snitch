1f7bb6d38997fa7b5ca451e31cb73c186ea26ed2
Dylan Jhaveri <dylanjhaveri@gmail.com>
2019-08-12 22:26:01 -0700

    hover, pop out snitch TV icon in the nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "bootstrap"
 import css from "../css/app.scss"
 import "phoenix_html"
 
